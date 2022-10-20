dbffe5fd88d5febbb406b226948d87aa0485f63f
Marcel <marcelclavero94@gmail.com>
2022-10-20 20:45:07 +0200

    fix env variable index

--- server/index.js
+++ server/index.js
@@ -2,10 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const staticFilesPath = path.resolve(
-  __dirname,
-  'process.env.STATIC_FILES_PATH'
-);
+const staticFilesPath = path.resolve(__dirname, process.env.STATIC_FILES_PATH);
 app.use('/', express.static(staticFilesPath));
 
 const PORT = process.env.PORT || 8081;
